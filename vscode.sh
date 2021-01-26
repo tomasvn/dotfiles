c5be8bd527aba1503020facf32315eceebfeb00d
tomasvn <nguyenngoct2112@gmail.com>
2021-01-26 13:15:28 +0100

    Update sh script

--- vscode.sh
+++ vscode.sh
@@ -4,13 +4,13 @@
 
 # Declare array of extensions
 declare -a arr=(
-  "CoenraadS.bracket-pair-colorizer"
-  "dbaeumer.vscode-eslint"
-  "mrmlnc.vscode-scss"
-  "whatwedo.twig"
-  "wix.vscode-import-cost"
-  "stylelint.vscode-stylelint"
-  "esbenp.prettier-vscode"
+  CoenraadS.bracket-pair-colorizer
+  dbaeumer.vscode-eslint
+  mrmlnc.vscode-scss
+  whatwedo.twig
+  wix.vscode-import-cost
+  stylelint.vscode-stylelint
+  esbenp.prettier-vscode
 )
 
 for ext in 
