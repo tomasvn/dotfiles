ff6da6b601db56191a9fe55a448a44dafa93e0f9
Ngoc Tu NGUYEN <nguyenngoct2112@gmail.com>
2024-03-31 21:00:31 +0200

    fix(vscode.sh): indentations

--- vscode.sh
+++ vscode.sh
@@ -5,30 +5,30 @@
 
 # Declare array of extensions
 declare -a arr=(
-	dbaeumer.vscode-eslint
-	SomewhatStationery.some-sass
-	wix.vscode-import-cost
-	stylelint.vscode-stylelint
-	esbenp.prettier-vscode
-	Orta.vscode-twoslash-queries
-	Vue.volar
-	teabyii.ayu
-	eamodio.gitlens
-	pflannery.vscode-versionlens
-	PKief.material-icon-theme
+    dbaeumer.vscode-eslint
+    SomewhatStationery.some-sass
+    wix.vscode-import-cost
+    stylelint.vscode-stylelint
+    esbenp.prettier-vscode
+    Orta.vscode-twoslash-queries
+    Vue.volar
+    teabyii.ayu
+    eamodio.gitlens
+    pflannery.vscode-versionlens
+    PKief.material-icon-theme
 )
 
 extensions=$(code --list-extensions)
 
 for ext in $arr
 do
-	# Run pattern matching with flag quite and ignore give case
-	if echo "$extensions" | grep -qi "^$ext"; then
-		echo "$extensions is already installed. Skipping..."
-	else
-		echo "Installing $extensions..."
-		code --install-extension "$ext"
-	fi
+    # Run pattern matching with flag quite and ignore give case
+    if echo "$extensions" | grep -qi "^$ext"; then
+        echo "$extensions is already installed. Skipping..."
+    else
+        echo "Installing $extensions..."
+        code --install-extension "$ext"
+    fi
 done
 
 echo "VS Code extensions have been installed."
