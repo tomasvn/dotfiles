ec6ba34388a94d11d5144c6505b1ced8487b50b8
tomasvn <nguyenngoct2112@gmail.com>
2024-10-22 23:04:32 +0200

    Update setup-pkgs.ps1

--- powershell/scripts/setup-pkgs.ps1
+++ powershell/scripts/setup-pkgs.ps1
@@ -1,27 +1,48 @@
 # Add packages from https://community.chocolatey.org/packages
-# Add only verified packages as the code below will install it
-# without user confirm
 $packages = @(
-    'googlechrome'
-    'git'
-    'vscode'
-    'spotify'
-    'powertoys'
-    'devtoys'
-    'powershell-core'
-    'visualstudio2022enterprise'
-    'lazygit'
-    # 'chezmoi'
+    'googlechrome',
+    'git',
+    'vscode',
+    'spotify',
+    'powertoys',
+    'devtoys',
+    'powershell-core',
+    'visualstudio2022enterprise',
+    'lazygit',
     'dotnet-6.0-aspnetruntime',
     'cmder',
     'poshgit'
 )
 
-$packages | ForEach-Object {
-    # Run command choco install
-    # with flag -y (--yes)
-    # $_ is current item in loop
-    choco install -y $_
-    $installPath = (Get-Command $_).Source
-    Write-Host "Package $_ installed at: $installPath"
+# Function to prompt for installation
+function Prompt-InstallPackage {
+    param (
+        [string]$package
+    )
+
+    $response = Read-Host -Prompt "Do you want to install $package? (y/n)"
+    if ($response -eq 'y') {
+        Write-Host "Installing $package..."
+        choco install -y $package
+        $installPath = (Get-Command $package).Source
+        Write-Host "Package $package installed at: $installPath"
+    } else {
+        Write-Host "Skipping $package..."
+    }
+}
+
+# Prompt to install all packages
+$responseAll = Read-Host -Prompt "Do you want to install all packages? (y/n)"
+if ($responseAll -eq 'y') {
+    foreach ($package in $packages) {
+        Write-Host "Installing $package..."
+        choco install -y $package
+        $installPath = (Get-Command $package).Source
+        Write-Host "Package $package installed at: $installPath"
+    }
+} else {
+    # Loop through each package and prompt for installation
+    foreach ($package in $packages) {
+        Prompt-InstallPackage -package $package
+    }
 }
