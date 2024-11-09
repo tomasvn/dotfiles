779b10f0666b3a701d54eaf0d78b68c1853e7143
Ngoc Tu NGUYEN <nguyenngoct2112@gmail.com>
2024-11-09 12:14:50 +0100

    fix(setup-profile.ps1): adjust script to setup custom profile

--- powershell/scripts/setup-profile.ps1
+++ powershell/scripts/setup-profile.ps1
@@ -1,3 +1,10 @@
+# Check if running as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Restart script with elevated privileges
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
 $source = ".\profile.ps1"
 $destination = [System.Environment]::GetFolderPath('MyDocuments') + "\WindowsPowerShell\profile.ps1"
 
@@ -9,4 +16,20 @@ if (Test-Path $source) {
 }
 else {
     Write-Output "Source file does not exist."
+
+    # Prompt to create the file
+    $response = Read-Host -Prompt "Source file does not exist. Do you want to create it? (Y,y / N,n)"
+
+    if ($response -eq "Y" -or $response -eq "y") {
+        # Create the file and copy the custom profile.ps1
+        New-Item -ItemType File -Path $source -Force
+        Copy-Item -Path $source -Destination $destination
+        Write-Output "File created and copied successfully."
+    }
+    elseif ($response -eq "N" -or $response -eq "n") {
+        Write-Output "Please create the source file manually and copy it over."
+    }
+    else {
+        Write-Output "Invalid response. Please enter Y,y or N,n."
+    }
 }
